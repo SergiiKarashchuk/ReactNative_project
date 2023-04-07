41f8054a3fc5cae72cedb0898e13f7943af8a080
Сергей Каращук <kspost01@gmail.com>
2023-04-07 16:56:35 +0300

    bag path

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import RegistrationScreen from "./src/components/Screens/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/bg.jpg")}
+      >
+        <RegistrationScreen />
+        <Text>Open up App.js to start working on your app!</Text>
+        {/* <StatusBar style="auto" /> */}
+      </ImageBackground>
     </View>
   );
 }
