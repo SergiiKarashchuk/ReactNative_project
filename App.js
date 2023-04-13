92515c547842be2bc494b3a81f4a5a01fb9070e8
Сергей Каращук <kspost01@gmail.com>
2023-04-13 23:39:41 +0300

    hw2

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-// import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import RegistrationScreen from "./src/components/Screens/RegistrationScreen";
-// import LoginScreen from "./src/components/Screens/LoginScreen";
+// import { StatusBar } from 'expo-status-bar';
+import LoginScreen from "./src/screens/authScreen/LoginScreen";
+import RegisterScreen from "./src/screens/authScreen/RegisterScreen";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+if (__DEV__) {
+  import("./ReactotronConfig").then(() => console.log("Reactotron Configured"));
+  console.log("I am in debug");
+}
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
+  //fonts
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./src/assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./src/assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./src/assets/fonts/Roboto/Roboto-Bold.ttf"),
+    "Lora-regular": require("./src/assets/fonts/Lora/static/Lora-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  // End fonts
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./src/images/photo-bg.jpg")}
-      >
-        <RegistrationScreen />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <RegisterScreen />
         {/* <LoginScreen /> */}
-        <Text>Open up App.js to start working on your app!</Text>
-        {/* <StatusBar style="auto" /> */}
-      </ImageBackground>
+      </NavigationContainer>
     </View>
   );
 }
@@ -22,13 +48,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
   },
 });
